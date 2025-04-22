64d3401daa39a492ca59b00ecf4f064bdc6c3e38
ExplosiveHDYT <82344578+ExplosiveHDYT@users.noreply.github.com>
2025-04-22 01:19:20 -0400

    Create quarry-installer.lua

---
+++ quarry-installer.lua
@@ -0,0 +1,23 @@
+-- installer.lua
+
+local url = {
+  main = "https://raw.githubusercontent.com/ExplosiveHDYT/quarry/refs/heads/main/main.lua",
+  moveX = "https://raw.githubusercontent.com/ExplosiveHDYT/quarry/refs/heads/main/moveX.lua",
+  moveY = "https://raw.githubusercontent.com/ExplosiveHDYT/quarry/refs/heads/main/moveY.lua",
+  moveZ = "https://raw.githubusercontent.com/ExplosiveHDYT/quarry/refs/heads/main/moveZ.lua",
+  quarryData = "https://raw.githubusercontent.com/ExplosiveHDYT/quarry/refs/heads/main/quarryData.lua"
+}
+
+print("Package?")
+local name = read()
+
+print("Install " .. name .. " as startup.lua? (y/n)")
+if read() == "y" then
+  if fs.exists("startup.lua") then
+    fs.delete("startup.lua")
+  end
+  shell.run("wget", url[name], "startup.lua")
+  print("Done.")
+else
+  print("Cancelled.")
+end
